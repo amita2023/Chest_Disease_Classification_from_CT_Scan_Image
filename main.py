0b551ec846b53408021189993230eff63559c9cf
Amita Sahoo <amita.sahoo023@gmail.com>
2024-05-29 00:29:27 -0500

    data ingestion

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+sys.path.append('/Users/amita/Projects/Chest_Disease_Classification_from_CT_Scan_Image/src/')
+
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
