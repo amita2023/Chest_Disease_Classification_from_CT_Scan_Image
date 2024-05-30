3cd97b03227e1c70b7f47c368e861ff1ccfd7180
Amita Sahoo <amita.sahoo023@gmail.com>
2024-05-30 12:23:57 -0500

    prepare base model

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ sys.path.append('/Users/amita/Projects/Chest_Disease_Classification_from_CT_Scan
 
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_modsel import PrepareBaseModelTrainingPipeline
+
+
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -14,5 +17,18 @@ try:
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Prepare Base Model"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
         logger.exception(e)
         raise e
